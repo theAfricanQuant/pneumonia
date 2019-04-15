940fb86781cd69b8cbfde9358111d1cf97409b2e
Ricky Macharm <33702921+RickyMacharm@users.noreply.github.com>
2019-04-15 15:28:53 +0100

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-l1SCdCsghzXykIdtBPWnTbEeZHvM2ps'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Healthy', 'sick']
 path = Path(__file__).parent
 
 app = Starlette()
